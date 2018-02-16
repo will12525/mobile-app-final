dee5ddc12e3e3bbb1658331ca59a123c359d5d0c
Karen Camaso <camasok@wit.edu>
2018-02-16 00:10:55 -0500

    Reworked Cards

--- app/src/main/java/edu/wit/mobileapp/mobile_app_final_project/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/mobile_app_final_project/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        RecyclerView rv = (RecyclerView) findViewById(R.id.attackList);
+        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+        rv.setLayoutManager(llm);
+
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
