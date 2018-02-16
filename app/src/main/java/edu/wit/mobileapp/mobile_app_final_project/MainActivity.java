97f9ae6fd9dedbc855b5f4f9de7719e8b38eca6e
wlawrenc <will.lawrence@analog.com>
2018-02-15 23:03:08 -0500

    hamburger bars

--- app/src/main/java/edu/wit/mobileapp/mobile_app_final_project/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/mobile_app_final_project/MainActivity.java
@@ -1,22 +1,72 @@
 package edu.wit.mobileapp.mobile_app_final_project;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout mDrawerLayout;
+    ListView mDrawerListView;
+    RelativeLayout mDrawerRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Button menuLeft =  findViewById(R.id.menuLeft);
+        Button menuRight =  findViewById(R.id.menuRight);
+
+        menuLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeToast("left toggled");
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    drawer.openDrawer(GravityCompat.START);
+                }
+
+                if(drawer.isDrawerOpen(GravityCompat.END)){
+                    drawer.closeDrawer(GravityCompat.END);
+                }
+            }
+        });
+
+        menuRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeToast("right toggled");
+                if (drawer.isDrawerOpen(GravityCompat.END)) {
+                    drawer.closeDrawer(GravityCompat.END);
+                } else {
+                    drawer.openDrawer(GravityCompat.END);
+                }
+
+                if(drawer.isDrawerOpen(GravityCompat.START)){
+                    drawer.closeDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        NavigationView navigationView1 =  findViewById(R.id.nav_view);
+        NavigationView navigationView2 =  findViewById(R.id.nav_view2);
+        navigationView1.setNavigationItemSelectedListener(this);
+        navigationView2.setNavigationItemSelectedListener(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+      //  getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -44,9 +94,23 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            makeToast("settings picked");
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void makeToast(String message){
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+      //  toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
+        toast.show();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        makeToast(item.toString());
+        return false;
     }
 }
