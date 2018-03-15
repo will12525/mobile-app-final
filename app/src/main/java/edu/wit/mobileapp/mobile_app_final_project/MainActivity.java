5dc1151f84612682c958f8b2607b2d5e47ddc4f9
Kcamaso <kcamaso@gmail.com>
2018-03-15 17:35:21 -0400

    Cleaned up Main Activity

--- app/src/main/java/edu/wit/mobileapp/mobile_app_final_project/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/mobile_app_final_project/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         mCharacterDataAdapter = new CharacterDataAdapter(this,0, list);
         listView.setAdapter(mCharacterDataAdapter);
 
-       // combatSetup();
+
 
     }
 
@@ -66,81 +66,6 @@ public class MainActivity extends AppCompatActivity {
         return db;
     }
 
-    public void combatSetup()
-    {
-        RecyclerView rv = (RecyclerView) findViewById(R.id.attackList);
-        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
-        rv.setLayoutManager(llm);
-
-        List<BattleItem> battleItemListing = new ArrayList<BattleItem>();
-
-        battleItemListing.add(new BattleItem("Xtreme Teen Bible","1d8", "holy", true));
-        battleItemListing.add(new BattleItem("Umbra Staff","1d12", "LUP", true));
-        battleItemListing.add(new BattleItem("Railsplitter","1d20", "Trees", false));
-        battleItemListing.add(new BattleItem("Flaming Raging Poisoning Sword of Doom","2d12", "Taako", false));
-
-        battleItemListing.add(new BattleItem("Magic Missle","3d4","Abraka fuck you!",true));
-
-
-        BattleItemAdapter adapter = new BattleItemAdapter(battleItemListing);
-        rv.setAdapter(adapter);
-
-        final EditText hpNumber = (EditText) findViewById(R.id.hpNumber);
-        final ProgressBar hpBar = (ProgressBar) findViewById(R.id.hpBar);
-        hpBar.setMax(100);
-        hpBar.setProgress(50,true);
-
-        hpNumber.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                //intentionally blank
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.toString().equals("") || s.toString().equals("-"))
-                {
-                    // hpBar.setProgress(0,true);
-                    // hpNumber.setText("0");
-                    // hpNumber.setSelection(1);
-                }else
-                {
-
-                    hpBar.setProgress(Integer.parseInt(s.toString()),true);
-
-                    int hp = Integer.parseInt(s.toString());
-
-
-
-                    if(hp >= 100)
-                    {
-                        hpBar.setProgressTintList(ColorStateList.valueOf(Color.BLUE));
-                    }
-                    if( hp <= 99 && hp > 50)
-                    {
-                        hpBar.setProgressTintList(ColorStateList.valueOf(Color.GREEN));
-                    }
-                    if( hp < 50 && hp >= 26)
-                    {
-                        hpBar.setProgressTintList(ColorStateList.valueOf(Color.argb(255,255,140,0))); // Orange
-                    }
-                    if(( hp <= 25))
-                    {
-                        hpBar.setProgressTintList(ColorStateList.valueOf(Color.RED));
-                    }
-
-
-
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                //intentionally blank
-            }
-        });
-    }
 
 
 
